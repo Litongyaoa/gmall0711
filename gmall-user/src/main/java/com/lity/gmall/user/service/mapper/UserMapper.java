85f2397397d25c47332c0178d327e5f919563cc8
lLty <852295972@qq.com>
2019-08-01 00:15:43 +0800

    test

---
+++ gmall-user/src/main/java/com/lity/gmall/user/service/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.lity.gmall.user.service.mapper;
+
+
+import com.lity.gmall.bean.UserInfo;
+import tk.mybatis.mapper.common.Mapper;
+
+public interface UserMapper extends Mapper<UserInfo> {
+}
